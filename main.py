c8d85c2a79f6b72f77390d6e0ad61df71b3b679f
Kunal-Chauhan <rismvkc@gmail.com>
2020-10-17 16:30:42 +0530

    Bug fixation

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
     while True:
 
         # Checking for the wake word/phrase
-        query = takeCommand().lower()
+        query = takeAudio().lower()
 
         if 'open youtube' in query:
             webbrowser.open("youtube.com")
